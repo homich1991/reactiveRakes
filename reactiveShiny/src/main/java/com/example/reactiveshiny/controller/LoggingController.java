7e79ecc632151f78bab8d8f4c92c4aaa7d65abb9
Roman Meerson <roman.meerson@payconiq.com>
2022-09-01 18:53:21 +0200

    set init for log

--- reactiveShiny/src/main/java/com/example/reactiveshiny/controller/LoggingController.java
+++ reactiveShiny/src/main/java/com/example/reactiveshiny/controller/LoggingController.java
@@ -2,20 +2,13 @@ package com.example.reactiveshiny.controller;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
-
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
 @Slf4j
 @RestController
@@ -41,9 +34,6 @@ public class LoggingController {
                 .retrieve()
                 .toEntity(String.class);
     }
-
-
-
 
 
 }
