091a63520b6136d00eaadd885c23ae974690a823
Roman Meerson <roman.meerson@payconiq.com>
2022-09-01 19:22:37 +0200

    set init for log

--- reactiveShiny/src/main/java/com/example/reactiveshiny/controller/LoggingController.java
+++ reactiveShiny/src/main/java/com/example/reactiveshiny/controller/LoggingController.java
@@ -3,7 +3,6 @@ package com.example.reactiveshiny.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
