7b567078fe7b252e685e37482c7abb222ff1c8a6
Roman Meerson <roman.meerson@payconiq.com>
2022-09-01 18:43:48 +0200

    set init for sink

--- reactiveShiny/src/main/java/com/example/reactiveshiny/controller/UpdateGeneratorController.java
+++ reactiveShiny/src/main/java/com/example/reactiveshiny/controller/UpdateGeneratorController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -15,20 +14,18 @@ import reactor.core.publisher.Flux;
 @RequestMapping("/api/reactive")
 public class UpdateGeneratorController {
 
-    @GetMapping(value = "/update", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<String> getUpdates() {
-        throw new NotImplementedException();
-    }
-
-
-    @PostMapping("/update")
-    public void setUpdate() {
-        addNewUpdate();
-    }
-
     private void addNewUpdate() {
         throw new NotImplementedException();
     }
 
+    @GetMapping("/add-update")
+    public void setUpdate() {
+        addNewUpdate();
+    }
+
+    @GetMapping(value = "/update", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> getUpdates() {
+        throw new NotImplementedException();
+    }
 
 }
