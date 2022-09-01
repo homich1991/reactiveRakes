f1c32fd18321d2d3ad253c3ccd5d083f2eae2e08
Roman Meerson <roman.meerson@payconiq.com>
2022-09-01 18:26:59 +0200

    set init for sink

--- reactiveShiny/src/main/java/com/example/reactiveshiny/controller/UpdateGeneratorController.java
+++ reactiveShiny/src/main/java/com/example/reactiveshiny/controller/UpdateGeneratorController.java
@@ -2,51 +2,32 @@ package com.example.reactiveshiny.controller;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Sinks;
-
-import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/reactive")
 public class UpdateGeneratorController {
 
-    Sinks.Many<String> updates;
-
-    final AtomicLong counter;
-
-
-    public UpdateGeneratorController() {
-        this.counter = new AtomicLong();
-        updates = Sinks.many().multicast().directAllOrNothing();
-        addNewUpdate();
-        addNewUpdate();
-        addNewUpdate();
-    }
-
-    @GetMapping(value = "/updates", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(value = "/update", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> getUpdates() {
-        return updates.asFlux();
+        throw new NotImplementedException();
     }
 
 
-    @GetMapping("/add-updates")
+    @PostMapping("/update")
     public void setUpdate() {
         addNewUpdate();
     }
 
-
     private void addNewUpdate() {
-        Sinks.EmitResult result = updates.tryEmitNext("Hello World #" + counter.getAndIncrement());
-
-        if (result.isFailure()) {
-            System.out.println("Fail to emit next");
-        }
+        throw new NotImplementedException();
     }
 
 
